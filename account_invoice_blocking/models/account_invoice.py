482384a58d537b5cd54405dfb01b666322f54c93
Benjamin Willig <benjamin.willig@acsone.eu>
2017-12-22 16:14:17 +0100

    Revert "[IMP] Set 'blocked' invoice field as stored to allow to search"
    This reverts commit c166f8a1fba4d4e3c441a4f02a1a2e65e3f7ae5a.

--- account_invoice_blocking/models/account_invoice.py
+++ account_invoice_blocking/models/account_invoice.py
@@ -28,16 +28,13 @@ class AccountInvoice(models.Model):
     def _update_blocked(self, value):
         """
         This method updates the boolean field 'blocked' of the move line
-        of the passed invoice with the passed value. If the invoice is in
-        draft (=no journal entry), we update the 'draft_blocked' invoice field
+        of the passed invoice with the passed value
         :param value: value to set to the 'blocked' field of the move line
         """
         self.ensure_one()
         if self.move_id:
             move_line_ids = self._get_move_line()
             move_line_ids.write({'blocked': value})
-        else:
-            self.write({'draft_blocked': value})
 
     @api.multi
     def _set_move_blocked(self):
@@ -61,20 +58,15 @@ class AccountInvoice(models.Model):
             invoice._update_blocked(invoice.draft_blocked)
         return res
 
-    @api.depends(
-        'draft_blocked',
-        'move_id.line_ids.blocked',
-        'move_id.line_ids.account_id.user_type_id',
-    )
+    @api.depends('move_id')
     def _get_move_blocked(self):
         """
         This method set the value of the field 'invoice.blocked' to True
-        If every line of the invoice's move is actualy blocked or if the
-        invoice is in draft and has been blocked.
+        If every line of the invoice's move is actualy blocked
         """
         for invoice in self:
             if not invoice.move_id:
-                invoice.blocked = invoice.draft_blocked
+                invoice.blocked = False
                 continue
 
             move_lines = invoice._get_move_line()
@@ -83,22 +75,10 @@ class AccountInvoice(models.Model):
 
     blocked = fields.Boolean(
         string='No Follow-up',
+        states={'draft': [('readonly', True)]},
         compute='_get_move_blocked',
-        inverse='_set_move_blocked',
-        store=True,
-    )
+        inverse='_set_move_blocked')
 
     draft_blocked = fields.Boolean(
         string='No Follow-up',
-        help="This flag facilitates the blocking of the invoice's move lines.",
-        copy=False,
-    )
-
-    @api.model
-    def fields_get(self, allfields=None, attributes=None):
-        res = super(AccountInvoice, self).fields_get(
-            allfields=allfields, attributes=attributes)
-        # Remove the draft blocked field from the custom search view
-        if 'draft_blocked' in res:
-            res['draft_blocked']['searchable'] = False
-        return res
+        help="This flag facilitates the blocking of the invoice's move lines.")
